da6eb15331de44620bd739f007e16d2f96c107ef
Siddhanth Pranav Rao <siddhanthprao@gmail.com>
2025-05-22 12:34:32 +0530

    DSA Practice

---
+++ Step 1 - Learn the Basics/Pattern13.java
@@ -0,0 +1,17 @@
+public class Pattern13 {
+
+    public static void createPattern(int n) {
+        int counter = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        createPattern(5);
+    }
+}
