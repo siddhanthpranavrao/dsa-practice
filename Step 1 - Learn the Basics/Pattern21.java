b3d662f85e9920414d58acc562db5f0e6c40302c
Siddhanth Pranav Rao <siddhanthprao@gmail.com>
2025-05-23 07:41:04 +0530

    DSA Practice

---
+++ Step 1 - Learn the Basics/Pattern21.java
@@ -0,0 +1,20 @@
+public class Pattern21 {
+    
+    public static void createPattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n-1 || j == 0 || j == n-1){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        createPattern(4);
+    }
+}
