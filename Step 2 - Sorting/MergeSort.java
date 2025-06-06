148ea2bc8730df8659900176bf78ae765c6748e4
siddhanthpranavrao <siddhanthprao@gmail.com>
2025-06-06 16:51:56 +0530

    DSA Practice

--- Step 2 - Sorting/MergeSort.java
+++ Step 2 - Sorting/MergeSort.java
@@ -1,52 +1,57 @@
-import java.util.ArrayList;
 
 public class MergeSort {
 
 	public static void sort(int[] arr) {
 		if (arr == null || arr.length < 2) return;
-		
+
 		mergeSort(arr, 0, arr.length - 1);
 	}
-	
+
 	public static void mergeSort(int[] arr, int low, int high) {
 		if (low >= high) return;
-		
-		int mid = (low + high) / 2;
+
+		int mid = low + (high - low) / 2;
 		mergeSort(arr, low, mid);
 		mergeSort(arr, mid+1, high);
 		merge(arr, low, mid, high);
-		
 	}
-	
-	public static void merge(int[] arr, int low, int mid , int high) {
-		ArrayList<Integer> temp = new ArrayList<>();
+
+	public static void merge(int[] arr, int low, int mid, int high) {
 		int left = low;
-		int right = mid + 1;
-		
+		int right = mid+1;
+		int[] temp = new int[high-low+1];
+		int pointer = 0;
+
 		while (left <= mid && right <= high) {
 			if (arr[left] <= arr[right]) {
-				temp.add(arr[left]);
+				temp[pointer] = arr[left];
+				pointer++;
 				left++;
 			} else {
-				temp.add(arr[right]);
+				temp[pointer] = arr[right];
+				pointer++;
 				right++;
 			}
 		}
-		
+
 		while (left <= mid) {
-			temp.add(arr[left]);
+			temp[pointer] = arr[left];
+			pointer++;
 			left++;
 		}
-		
+
 		while (right <= high) {
-			temp.add(arr[right]);
+			temp[pointer] = arr[right];
+			pointer++;
 			right++;
 		}
-		
+
 		for (int i = low; i <= high; i++) {
-			arr[i] = temp.get(i - low);
+			arr[i] = temp[i-low];
 		}
 	}
+
+	
 
 	public static void main(String[] args) {
 		int[] arr = {10,3,2,6,5,4,7,9,8,1};
