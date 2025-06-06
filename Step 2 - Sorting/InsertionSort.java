8fad447fa19a55940f601b07425d91f643316e24
siddhanthpranavrao <siddhanthprao@gmail.com>
2025-06-06 16:30:50 +0530

    DSA Practice

--- Step 2 - Sorting/InsertionSort.java
+++ Step 2 - Sorting/InsertionSort.java
@@ -3,16 +3,18 @@ public class InsertionSort {
 	public static void sort(int[] arr) {
 		
 		int n = arr.length;
-		for (int i = 0; i < n; i++) {
+
+		for (int i = 1; i < n; i++) {
 			int j = i;
-			
-			while (j > 0 && arr[j-1] > arr[j]) {
-				int temp = arr[j];
-				arr[j] = arr[j-1];
-				arr[j-1] = temp;
+			while (j > 0 && arr[j - 1] > arr[j]) {
+				int temp = arr[j-1];
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+
 				j--;
 			}
 		}
+		
 	}
 
 	public static void main(String[] args) {
