368871e70182189e45a395db1f3a5f78508a59f9
siddhanthpranavrao <siddhanthprao@gmail.com>
2025-06-06 18:05:29 +0530

    DSA Practice

--- Step 2 - Sorting/QuickSort.java
+++ Step 2 - Sorting/QuickSort.java
@@ -1,26 +1,34 @@
 public class QuickSort {
 
     public static void sort(int[] arr) {
-        if (arr == null || arr.length < 2) return;
+        if (arr == null || arr.length < 2)
+            return;
+
         quickSort(arr, 0, arr.length - 1);
     }
 
     public static void quickSort(int[] arr, int low, int high) {
-        if (low >= high) return;
+        if (low >= high)
+            return;
 
-        int pIndex = partition(arr, low, high);
+        int pIndex = pivot(arr, low, high);
         quickSort(arr, low, pIndex - 1);
         quickSort(arr, pIndex + 1, high);
     }
 
-    public static int partition(int[] arr, int low, int high) {
-        int pivot = arr[low];
-        int left = low + 1;
+    public static int pivot(int[] arr, int low, int high) {
+        int left = low;
         int right = high;
+        int pivot = arr[low];
 
-        while (left <= right) {
-            while (left <= high && arr[left] <= pivot) left++;
-            while (right >= low && arr[right] > pivot) right--;
+        while (left < right) {
+            while (arr[left] <= pivot && left <= high - 1) {
+                left++;
+            }
+
+            while (arr[right] > pivot && right >= low + 1) {
+                right--;
+            }
 
             if (left < right) {
                 int temp = arr[left];
@@ -29,7 +37,6 @@ public class QuickSort {
             }
         }
 
-        // swap pivot with arr[right]
         int temp = arr[low];
         arr[low] = arr[right];
         arr[right] = temp;
@@ -38,7 +45,7 @@ public class QuickSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {10, 3, 2, 6, 5, 4, 7, 9, 8, 1};
+        int[] arr = { 10, 3, 2, 6, 5, 4, 7, 9, 8, 1 };
         sort(arr);
         for (int a : arr) {
             System.out.println(a);
