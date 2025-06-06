df153d281378198b6845429a7e9e5265c50eca93
siddhanthpranavrao <siddhanthprao@gmail.com>
2025-06-06 16:20:44 +0530

    DSA Practice

--- Step 2 - Sorting/BubbleSort.java
+++ Step 2 - Sorting/BubbleSort.java
@@ -1,23 +1,26 @@
 public class BubbleSort {
 
 	public static void sort(int[] arr) {
-		
 		int n = arr.length;
-		
-		for (int i = 0; i < n; i++) {
+
+		for (int i = 0; i < n-1; i++) {
 			boolean swapped = false;
-			
 			for (int j = 0; j < n - i - 1; j++) {
 				if (arr[j] > arr[j+1]) {
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+					int temp = arr[j+1];
+					arr[j+1] = arr[j];
+					arr[j] = temp;
 					
 					swapped = true;
 				}
 			}
-			if (!swapped) break;
+
+			if (!swapped) {
+				break;
+			}
 		}
+		
+		
 	}
 
 	public static void main(String[] args) {
