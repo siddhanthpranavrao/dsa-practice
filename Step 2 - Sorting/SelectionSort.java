f687d49bae2b8494300064660e17eb331319f70c
siddhanthpranavrao <siddhanthprao@gmail.com>
2025-06-06 16:10:58 +0530

    DSA Practice

--- Step 2 - Sorting/SelectionSort.java
+++ Step 2 - Sorting/SelectionSort.java
@@ -1,11 +1,11 @@
 public class SelectionSort {
 	
-	public static void sort(int[] arr) { 
-
+	public static void sort(int[] arr) {
+		
 		int n = arr.length;
 		for (int i = 0; i < n; i++) {
 			int min = i;
-			for (int j = i; j < n; j++) {
+			for (int j = i + 1; j < n; j++) {
 				if (arr[min] > arr[j])
 					min = j;
 			}
