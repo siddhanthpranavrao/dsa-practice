537f13b2723d886c28eb08bdcd7c22fe03777434
siddhanthpranavrao <siddhanthprao@gmail.com>
2025-06-07 05:45:49 +0530

    DSA Practice

--- Step 2 - Sorting/QuickSort.java
+++ Step 2 - Sorting/QuickSort.java
@@ -1,48 +1,46 @@
 public class QuickSort {
 
-    public static void sort(int[] arr) {
-        if (arr == null || arr.length < 2)
-            return;
 
-        quickSort(arr, 0, arr.length - 1);
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+
+        quickSort(arr, 0, arr.length-1);
     }
 
     public static void quickSort(int[] arr, int low, int high) {
-        if (low >= high)
-            return;
+        if (low >= high) return;
 
         int pIndex = pivot(arr, low, high);
-        quickSort(arr, low, pIndex - 1);
-        quickSort(arr, pIndex + 1, high);
+        quickSort(arr, low, pIndex-1);
+        quickSort(arr, pIndex+1, high);
     }
-
+    
     public static int pivot(int[] arr, int low, int high) {
-        int left = low;
-        int right = high;
         int pivot = arr[low];
+        int l = low;
+        int r = high;
 
-        while (left < right) {
-            while (arr[left] <= pivot && left <= high - 1) {
-                left++;
-            }
+        while(l < r) {
+            while (l <= high - 1 && arr[l] <= pivot)
+                l++;
+            
+            while (r >= low + 1 && arr[r] > pivot)
+                r--;
 
-            while (arr[right] > pivot && right >= low + 1) {
-                right--;
-            }
-
-            if (left < right) {
-                int temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
+            if (l < r) {
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
             }
         }
 
-        int temp = arr[low];
-        arr[low] = arr[right];
-        arr[right] = temp;
+        int temp = arr[r];
+        arr[r] = arr[low];
+        arr[low] = temp;
 
-        return right;
+        return r;
     }
+
 
     public static void main(String[] args) {
         int[] arr = { 10, 3, 2, 6, 5, 4, 7, 9, 8, 1 };
