21f0df531d3ce3c32b817ee762debe6ae59a09e5
siddhanthpranavrao <siddhanthprao@gmail.com>
2025-06-10 05:53:06 +0530

    DSA Practice

--- Step 3 - Solving Problems on Array/1 - Easy/UnionArray.java
+++ Step 3 - Solving Problems on Array/1 - Easy/UnionArray.java
@@ -3,49 +3,46 @@ import java.util.ArrayList;
 public class UnionArray {
 
     public static ArrayList<Integer> unionArray(int[] a1, int[] a2) {
-        if (a1 == null || a2 == null)
-            return new ArrayList<Integer>();
+        // Use the two pointer method
+        int p1 = 0; int p2 = 0;
+        // Create the final union Array List
+        ArrayList<Integer> union = new ArrayList<>();
+        int n1 = a1.length; int n2 = a2.length;
 
-        if (a1.length < 1 || a2.length < 1)
-            return new ArrayList<Integer>();
-
-        int n1 = a1.length;
-        int n2 = a2.length;
-        int p1 = 0;
-        int p2 = 0;
-        ArrayList<Integer> res = new ArrayList<>();
-
+        // Loop over until one of them hits the boundary of the array
         while (p1 < n1 && p2 < n2) {
+            // if <= add from first array
             if (a1[p1] <= a2[p2]) {
-                if (res.size() == 0 || res.get(res.size() - 1) != a1[p1])
-                    res.add(a1[p1]);
+                if (union.size() == 0 || union.get(union.size() - 1) != a1[p1])
+                    union.add(a1[p1]);
                 p1++;
             } else {
-                if (res.size() == 0 || res.get(res.size() - 1) != a2[p2])
-                    res.add(a2[p2]);
+                if (union.size() == 0 || union.get(union.size() - 1) != a2[p2])
+                    union.add(a2[p2]);
                 p2++;
             }
         }
 
         while (p1 < n1) {
-            if (res.get(res.size() - 1) != a1[p1])
-                res.add(a1[p1]);
+            if (union.size() == 0 || union.get(union.size() - 1) != a1[p1])
+                union.add(a1[p1]);
             p1++;
         }
 
         while (p2 < n2) {
-            if (res.get(res.size() - 1) != a2[p2])
-                res.add(a2[p2]);
+            if (union.size() == 0 || union.get(union.size() - 1) != a2[p2])
+                union.add(a2[p2]);
             p2++;
         }
 
-        return res;
+        return union;
     }
 
     public static void main(String[] args) {
-        int[] arr1 = { 1, 2, 3, 4, 5 };
-        int[] arr2 = { 2, 3, 4, 4, 5 };
+        int[] arr1 = {1, 1, 2, 2, 3};
+        int[] arr2 = {2, 2, 3, 3, 4};
         ArrayList<Integer> union = unionArray(arr1, arr2);
+
 
         for (int u : union) {
             System.out.println(u);
